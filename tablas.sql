974191430fa60af31179b80c72c7f37ae2b3e008
rubenavila9 <56893148+rubenavila9@users.noreply.github.com>
2020-08-26 14:53:18 -0500

    Create  tablas.sql

---
+++ tablas.sql
@@ -0,0 +1,42 @@
+create table CLIENTE (
+   ID_CLIENTE            int                  not null,
+   ID_MEMBRESIA     int                 null,
+   NOMBRES_CLIENTE       varchar(50)          null,
+   APELLIDOS_CLIENTE     varchar(50)          null,
+   IDENTIFICACION_CLIENTE varchar(20)          null,
+   GENERO               varchar(20)          null,
+   TELEFONO_CONVENCIONAL varchar(50)          null,
+   TELEFONO_MOVIL        varchar(50)          null,
+   EMAIL_CLIENTE         varchar(30)          null,
+   constraint PK_CLIENTE primary key (ID_CLIENTE)
+)
+go
+create table MEMBRESIA (
+   ID_MEMBRESIA             int                  not null,
+   FECHA_EXPEDICION     varchar(50)        null,
+   FECHA_EXPIRACION     varchar(50)       null,
+   constraint PK_MEMBRESIA primary key (ID_MEMBRESIA)
+)
+go
+create table PELICULA (
+   ID_PELICULA          int                  not null,
+   TITULO_PELICULA         varchar(50)          null,
+   PAIS_PELICULA              varchar(30)          null,
+AÑO_PELICULA              int          null,
+GENERO_PELICULA              varchar(20)          null,
+IDIOMA_PELICULA              varchar(20)          null,
+   PRECIO_PELICULA   numeric(5,2)         null,
+   FECHA_ULTIMOALQUILER   varchar(50)             null,
+   constraint PK_PELICULA primary key (ID_PELICULA)
+)
+go
+create table ALQUILER_PELICULA (
+   ID_PELICULA          int                  not null,
+   ID_CLIENTE            int                  not null,
+   ID_ALQUILER_PELICULA           int                  not null,
+   FECHAENTREGA_PELICULA  varchar(50)    null,
+   FECHASALIDA_PELICULA  varchar(50)    null,
+   constraint PK_ALQUILER_PELICULA primary key (ID_PELICULA, ID_CLIENTE,
+ ID_ALQUILER_PELICULA)
+)
+go
